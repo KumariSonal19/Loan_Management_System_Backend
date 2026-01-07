7a7cccc24d41518ea7d31e026300ea6f98318e1c
KumariSonal19 <sonalkumari5858@gmail.com>
2026-01-07 06:17:37 +0530

    Updated emi-service

---
+++ emi-service/src/test/java/com/lms/emi/EmiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.emi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
